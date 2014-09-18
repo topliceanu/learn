04b391ff3f4280c6564a34000d99fa5c87627def
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2014-09-19 00:20:18 +0300

    implement and test the closest pair algorithm

--- algo/src/closest_pair.py
+++ algo/src/closest_pair.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import math
+
+
 def closest_pair(points):
     """ Computes the closest pair from a given set of points in a plain.
 
@@ -21,6 +24,74 @@ def closest_pair(points):
             of the points analyzed.
 
     Returns:
-        A tuple of tuples representing hte closest pair of points.
+        A tuple of tuples representing the closest pair of points.
     """
-    pass
+    Px = sorted(list(points), key=lambda x: x[0])
+    Py = sorted(list(points), key=lambda x: x[1])
+
+    return closest_pair_rec(Px, Py)
+
+
+def closest_pair_rec(Px, Py):
+    """ Recursively compute the closest pair in both px and py sets of points.
+
+    Args:
+        px: list of tuples [(x, y)] sorted by x
+        py: list of tuples [(x, y)] sorted by y
+
+    Returns:
+        A tuple with the closest pair of points format ((x1, y1), (x2, y2)).
+    """
+    if len(Px) < 4:
+        # This is the base case.
+        pairs = []
+        for i in range(len(Px) -1):
+            for j in range(i+1, len(Px)):
+                pairs.append((Px[i], Px[j]))
+        pair = min(pairs, key=lambda p: distance(p[0], p[1]))
+        return pair
+
+
+    # Split points in two: left and right sorted by coordinate.
+    Qx = Px[len(Px)/2:]
+    Rx = Px[:len(Px)/2]
+    Qy = Py[len(Py)/2:]
+    Ry = Py[:len(Py)/2]
+
+    # Apply closest pair on both subsets.
+    (p1, q1) = closest_pair_rec(Qx, Qy)
+    (p2, q2) = closest_pair_rec(Rx, Ry)
+
+    # Compute closest pair of subsets and split.
+    tmp_pair = min([(p1, q1), (p2, q2)], key=lambda p: distance(p[0], p[1]))
+    delta = distance(tmp_pair[0], tmp_pair[1])
+    split_pair = closest_split_pair(Px, Py, delta)
+
+    if split_pair is None:
+        return tmp_pair
+    else:
+        return split_pair
+
+def closest_split_pair(Px, Py, delta):
+    """ Subroutine which checks if there is a pair of point such that one
+    point is in Px, the other is in Py and the distance between them is less
+    than delta.
+
+    Returns:
+        None if no split pair is discovered which w/ dist smaller than delta.
+        A tuple of points (tuples) representing point w/ smallest distance.
+    """
+    big_x = Px[-1] # biggest element in the left of Px.
+    Sy = [x for x in Px if x[0] >= big_x[0] - delta or x[0] <= big_x[0] + delta]
+    best = delta
+    best_pair = None
+    for i in range(len(Sy) - 7):
+        for j in range(7):
+            if distance(Sy[i], Sy[j]) < best:
+                best = distance(Sy[i], Sy[j])
+                best_pair = (Sy[i], Sy[j])
+    return best_pair
+
+def distance(p, q):
+    """ Computes the distance between two points p and q."""
+    return math.sqrt((p[0] - q[0])**2 + (p[1] - q[1])**2)
--- algo/test/test_closest_pair.py
+++ algo/test/test_closest_pair.py
@@ -2,8 +2,20 @@
 
 import unittest
 
+from src.closest_pair import closest_pair
+
 
 class TestClosestPair(unittest.TestCase):
 
     def test_closest_pair(self):
-        pass
+        """ Compute the closest pair of a set of given pairs. """
+        points = [(1,1), (1,2), (1,4), (1,6), (1, 10), (1,16), (1,26)]
+        actual = closest_pair(points)
+        expected = [(1,1), (1,2)]
+        self.assertItemsEqual(actual, expected, 'compute expected closest points')
+
+    def test_closest_non_obvious_pair(self):
+        points = [(9, 3), (-2, -2), (6, 4), (-6, -1), (3, 5), (-4, 2)]
+        actual = closest_pair(points)
+        expected = [(3,5), (6,4)]
+        self.assertItemsEqual(actual, expected, 'compute expected closest points')
