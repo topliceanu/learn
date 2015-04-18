8ab9c446cc2ee9a5c62f32b76f4e6452fce09774
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-04-18 15:05:02 +0300

    add time and space optimizations to bellman-ford algorithm implementation and also also add option to controll whether the actual paths should be computed or not

--- algo/src/bellman_ford_shortest_path.py
+++ algo/src/bellman_ford_shortest_path.py
@@ -3,7 +3,7 @@
 from src.graph import Graph
 
 
-def shortest_path(graph, s):
+def shortest_path(graph, s, return_paths=True):
     """ Implement the single source shortest path problem using the bellman-ford
     algorithm.
 
@@ -18,24 +18,27 @@ def shortest_path(graph, s):
 
     Params:
         graph: object, instance of src.graph.Graph, structure representing a graph.
-        s, int, the key of the starting vertex in the graph.
+        s: int, the key of the starting vertex in the graph.
+        return_paths: bool, returns the shortest paths also.
 
     Returns:
         tuple, format (costs, paths)
             costs: dict, format {destination_vertex: cost_to_that_vertex}
             paths: dict, format {destination_vertex: [edges_to_that_vertex]}
-
-        OR a list of edges which make up a negative length cycle.
+                Only available if returns_paths=True, otherwise is an empty dict.
     """
     # 0. Initialization:
     # A[i][v] - cost of shortest path from start vertex s to v using at most
     # i edges in the path. (i has values from 0 to n edges)
     # B[i][v] - holds the second to last vertex on the shortest path from s to
     # v with at most i edges OR None if no such path exists.
+    # We only need the last two rows or A and the last row of B.
     vertices = graph.get_vertices()
     n = len(vertices)
-    A = [[0]*n for __ in range(n+1)]
-    B = [[None]*n for __ in range(n+1)]
+    #A = [[0] * n for __ in range(n+1)]
+    #B = [[None] * n for __ in range(n+1)]
+    A = [[0] * n for __ in range(2)]
+    B = [None] * n
 
     # s - the name of the vertex s; pos_s - the position of vertex in the list.
     pos_s = vertices.index(s)
@@ -56,6 +59,10 @@ def shortest_path(graph, s):
     # L(i,v) = min( L(i-1,v), min {L(i-1,w)+c(v,w)} )
     #                        (w,v)
     for i in range(1, n+1):
+        if i > 1:
+            # Move the last computed row of path costs for the next computation.
+            A[0] = A[1]
+            A[1] = [0] * n
         for pos_v in range(n):
             v = vertices[pos_v]
             # If there are no shortest paths from s to v (perhaps because
@@ -64,31 +71,42 @@ def shortest_path(graph, s):
             min_pos_w = None
             for w in graph.incident(v):
                 pos_w = vertices.index(w)
-                tmp = A[i-1][pos_w] + graph.get_edge_value((w, v))
+                #tmp = A[i-1][pos_w] + graph.get_edge_value((w, v))
+                tmp = A[0][pos_w] + graph.get_edge_value((w, v))
                 if tmp < min_case_two:
                     min_case_two = tmp
                     min_pos_w = pos_w
-            min_case_one = A[i-1][pos_v]
+            #min_case_one = A[i-1][pos_v]
+            min_case_one = A[0][pos_v]
 
-            A[i][pos_v] = min(min_case_one, min_case_two)
-            B[i][pos_v] = min_pos_w
+            #A[i][pos_v] = min(min_case_one, min_case_two)
+            A[1][pos_v] = min(min_case_one, min_case_two)
+            B[pos_v] = min_pos_w
+
+        # Optimization: when the two successive rows of computed min path costs
+        # are the same, then we can stop sooner.
+        #if i < n and A[0] == A[1]:
+        #    break
 
     # 3. Detect negative cycles by running the algorithm for i > n-1 and
     # checking if the shortest paths change value.
-    if A[n-1] != A[n]:
+    #if A[n-1] != A[n]:
+    if A[0] != A[1]:
         return False # graph has cycles with negative cost.
     else:
-        costs = dict(zip(vertices, A[n-1]))
+        costs = dict(zip(vertices, A[0]))
 
     # 4. Compute the paths as well from each vertex to every other vertex,
     # by using the B 2d array.
-    def rec_path(i):
-        if B[n-1][i] in [None, pos_s]:
-            return []
-        rest = rec_path(B[n-1][i])
-        rest.append(vertices[B[n-1][i]])
-        return rest
-    paths = {v: rec_path(i) for i, v in enumerate(vertices)}
+    paths = {}
+    if return_paths is True:
+        def rec_path(i):
+            if B[i] in [None, pos_s]:
+                return []
+            rest = rec_path(B[i])
+            rest.append(vertices[B[i]])
+            return rest
+        paths = {v: rec_path(i) for i, v in enumerate(vertices)}
 
     # 5. Return solution.
     return (costs, paths)
--- algo/test/test_bellman_ford_shortest_path.py
+++ algo/test/test_bellman_ford_shortest_path.py
@@ -27,7 +27,26 @@ class BellmanFordTest(unittest.TestCase):
         self.assertEqual(paths, expected_paths,
             'should return the correct minimum paths vertices')
 
-    def test_bellman_ford_with_non_negative_cycle(self):
+    def test_bellman_ford_without_computing_paths(self):
+        """ Given the following graph:
+           /--2-->(v)---2--->(w)--2--\v
+        (s)        1\v               (t)
+          \----4--->(x)------4------/^
+        """
+        g = Graph.build(edges=[
+                ('s', 'v', 2), ('s', 'x', 4), ('v', 'w', 2),
+                ('w', 't', 2), ('v', 'x', 1), ('x', 't', 4)
+            ],
+            directed=True)
+        (costs, paths) = shortest_path(g, 's', return_paths=False)
+        expected_costs = {'x': 3, 's': 0, 't': 6, 'w': 4, 'v': 2}
+        expected_paths = {} #
+        self.assertEqual(costs, expected_costs,
+            'should return the correct minimum paths costs')
+        self.assertEqual(paths, expected_paths,
+            'should not return any paths as instructed')
+
+    def test_bellman_ford_with_negative_cycle(self):
         """ Given the following graph:
            /--2-->(v)---2--->(w)--2--\v
         (s)        \^      /        (t)
@@ -42,3 +61,36 @@ class BellmanFordTest(unittest.TestCase):
             directed=True)
         no_negative_cycles = shortest_path(g, 's')
         self.assertFalse(no_negative_cycles, 'should detect no negative cycles')
+
+    def test_bellman_ford_with_non_negative_cycle(self):
+        """ Given the following graph from the lecture:
+                  -2
+            (a)-------->(b)
+            ^|           |
+             |           |
+             4\       -1/
+               \       /
+                \(c)</
+                 /  \
+               2/    \-3
+              v/      \v
+             (x)       (y)
+              \^       /^
+               \      /
+               1\    /-4
+                 \  /
+                 (z)
+        And and a vertex (s) that is connected to all other vertices through
+        edges of cost 0.
+        """
+        g = Graph.build(edges=[('a', 'b', -2), ('c', 'a', 4), ('b', 'c', -1),
+                ('c', 'x', 2), ('c', 'y', -3), ('z', 'x', 1), ('z', 'y', -4)],
+                directed=True)
+        g.add_vertex('s')
+        for vertex in g.get_vertices():
+            g.add_edge(('s', vertex, 0))
+
+        (costs, __) = shortest_path(g, 's', return_paths=False)
+        expected_costs = {'a': 0, 'c': -3, 'b': -2, 's': 0,'y': -6, 'x': -1,'z': 0}
+        self.assertEqual(costs, expected_costs,
+            'should produce the correct costs')
