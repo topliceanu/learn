cec691697af095a781d84b9afb26df061575cc2b
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-04-23 07:25:35 +0300

    move all string matching algorithms to the same file

--- algo/src/rabin_karp.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def rabin_karp(needle, hay_stack):
-    """ Function implements the Rabin-Karp string searching algorithm. """
--- algo/test/test_rabin_karp.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import unittest
-
-from src.rabin_karp import rabin_karp
-
-
-class RabinKarp(unittest.TestCase):
-    pass
