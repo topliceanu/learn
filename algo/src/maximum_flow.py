87bd410b4a9ffad8c01e3b63f14b46ac3825d85e
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-04-23 07:29:17 +0300

    better docs on the maximum flow problem

--- algo/src/maximum_flow.py
+++ algo/src/maximum_flow.py
@@ -5,13 +5,16 @@ def maximum_flow(g, s, t):
     """ Solves the maximum flow problem.
 
     Given a directed weighted graph g, a source vertex s and a sink vertex t,
-    send as much flow as possible from s to t, knowing that each edge's weight
-    is the flow capacity.
+    send as much flow as possible from s to t, knowing that each edge has a
+    capacity value which is the maximum ammount of flow that the edge can
+    accomodate.
+
+    Running time: O(m*n) where m - number of edges, n - number of vertices.
 
     Params:
         g: object, instance of src.graph.Graph
-        s: str, name of the input vertex
-        t: str, name of the output vertex
+        s: str, name of the source vertex
+        t: str, name of the sink vertex
     """
 
 def ford_fulkerson_maximum_flow(g, s, t):
