0f61f0643d298697ac60c7fb014408d478d5e1e0
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-05-04 07:03:29 +0300

    fix the lru cache implementation to be much faster by using linked lists

--- algo/src/cache.py
+++ algo/src/cache.py
@@ -4,80 +4,118 @@ import time
 
 
 class Cache(object):
-    """ Base abstract class for all cache eviction strategies.
-
-    This class' only concern is storing data and not overflowing.
-
-    Attrs:
-        data: dict, actual container of the data.
-        max_size: int, the number of keys the cache can hold.
-        size: int, how many key the cache currently holds.
-    """
+    """ Base abstract class for all cache eviction strategies."""
     def __init__(self, max_size):
-        self.data = {}
-        self.size = 0
+        if max_size <= 1:
+            raise Exception('Cache size should be more than 1')
         self.max_size = max_size
 
     def write(self, key, value):
-        """ Writes the data to cache, maintaining the space constraints. """
-        if self.size + 1 > self.max_size:
-            raise Exception('Cache size overflow')
+        """ Writes the data to cache.
 
-        self.data[key] = value
-        self.size += 1
+        Args:
+            key: str
+            value: any
+        """
+        raise Exception('Cache#write method must be implemented')
 
     def read(self, key):
         """ Reads data from the cache.
 
         Args:
             key: str
-
-        Raises:
-            Exception, when the key is not in the cache.
         """
-        if key in self.data:
-            return self.data[key]
-        else:
-            raise Exception('Cache miss for key {key}'.format(key=key))
+        raise Exception('Cache#read method must be implemented')
 
 
 class LRUCache(Cache):
     """ Implements the Last-Recently-Used cache eviction strategy.
 
-    Attrs:
-        ts: dict, a hash of timestamps of recent usage for each key in data.
-    """
+    The implementation uses a doubly linked list to keep the last recently used
+    keys last. Whenever a new key is read/written the corresponding node is
+    moved to the from of the list. This implementation also uses a hash table
+    whereby for each key there corresponds the nodes in the list.
 
+    Attrs:
+        data: dict, format {key: {key, value, previous, next}}
+        first: dict, first element in a doubly linked list representing priority.
+            Format {key, value, previous, next}
+        last: dict, last element in a doubly linked list representing priority.
+            Format {key, value, previous, next}
+    """
     def __init__(self, max_size):
         Cache.__init__(self, max_size)
-        self.ts = {}
+        self.data = {}
+        self.first = None
+        self.last = None
 
     def write(self, key, value):
         """ Writes a piece of data in the cache.
-
-        First it finds and evincts the oldest key in the cache, then inserts
-        the new one in it's place.
-
-        TODO: instead of always going through the entire timestamp dicts to
-        find the oldest timestamp, we can use a max-heap.
 
         Args:
             key: str
             value: anything
         """
-        if self.size + 1 > self.max_size: # No more have room in the cache.
-            oldest_ts = None
-            for k, ts in self.ts.iteritems():
-                if oldest_ts == None:
-                    oldest_ts = ts
-                    last_used_key = k
-                if ts < oldest_ts:
-                    oldest_ts = ts
-                    last_used_key = k
+        if len(self.data) == self.max_size and key not in self.data:
+            self.evict()
 
-            del self.data[last_used_key]
-            del self.ts[last_used_key]
-            self.size -= 1
+        if self.first == None: # First element in the cache.
+            node = {'key': key, 'value': value, 'next': None, 'previous': None}
+            self.data[key] = node
+            self.first = node
+            self.last = node
+        elif key not in self.data: # Key not already present.
+            node = {'key': key, 'value': value, 'next': self.first, 'previous': None}
+            self.first['previous'] = node
+            self.first = node
+            self.data[key] = node
+        else: # Key already present in the cache.
+            node = self.data[key]
+            if node['previous'] != None: # First node in the list already.
+                node['previous']['next'] = node['next']
+                if node['next'] == None: # When we have only two elements.
+                    self.last = self.first
+                node['previous'] = None
+                node['next'] = self.first
+                self.first['previous'] = node
+                self.first = node
+            node['value'] = value
 
-        self.ts[key] = time.time()
-        return Cache.write(self, key, value)
+    def read(self, key):
+        """ Reads a piece of data from the cache.
+
+        Args:
+            key: str
+
+        Raises:
+            Exception, when a cache miss occurs.
+        """
+        if key not in self.data:
+            raise Exception('Cache miss for key {key}'.format(key=key))
+        node = self.data[key]
+
+        if node['previous'] != None: # It's not already the first element.
+            node['previous']['next'] = node['next']
+            if node['next'] == None: # Node was the last one.
+                self.last = self.first
+            node['previous'] = None
+            node['next'] = self.first
+            self.first['previous'] = node
+            self.first = node
+
+        value = node['value']
+        return value
+
+    def evict(self):
+        """ Removes the last recently used key from the cache. """
+        node = self.last
+        if node == None: # The cache has no elements.
+            return
+
+        if node['previous'] != None: # The cache has more than one element.
+            node['previous']['next'] = None
+            self.last = node['previous']
+        else: # The cache has only one element.
+            self.first = None
+            self.last = None
+        del self.data[node['key']]
--- algo/test/test_cache.py
+++ algo/test/test_cache.py
@@ -8,16 +8,109 @@ from src.cache import LRUCache
 
 class TestCache(unittest.TestCase):
 
+    def test_write_evicts_the_oldest_elem_when_cache_is_full(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.write('b', 20)
+        lru.write('c', 30)
+
+        self.assertEqual(len(lru.data), 2, 'should have only one element')
+        self.assertEqual(lru.first['key'], 'c', 'c is the most recent')
+        self.assertEqual(lru.last['key'], 'b', 'b is the latest addition')
+
+    def test_write_adds_only_element_to_cache(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        self.assertEqual(lru.first['key'], 'a', 'a is the first')
+        self.assertEqual(lru.last['key'], 'a', 'a is the last')
+
+    def test_write_moves_key_to_the_first_position(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.write('b', 20)
+        lru.write('a', 30)
+
+        self.assertEqual(lru.first['key'], 'a', 'a is the first')
+        self.assertEqual(lru.last['key'], 'b', 'b is the last')
+        self.assertEqual(lru.first['next']['key'], 'b', 'b is successor to a')
+        self.assertEqual(lru.data['a']['value'], 30, 'should have updated val')
+
+    def test_write_inserts_new_key_in_front_of_cache(self):
+        lru = LRUCache(3)
+        lru.write('a', 10)
+        lru.write('b', 20)
+        lru.write('c', 30)
+
+        self.assertEqual(lru.first['key'], 'c', 'c is the first')
+        self.assertIsNone(lru.first['previous'], 'a has no predecessor')
+        self.assertEqual(lru.first['next']['key'], 'b', 'a successor is b')
+        self.assertEqual(lru.data['b']['previous']['key'], 'c', 'c before b')
+        self.assertEqual(lru.data['b']['next']['key'], 'a', 'a after b')
+        self.assertEqual(lru.last['key'], 'a', 'a is the last')
+        self.assertEqual(lru.last['previous']['key'], 'b', 'b before last')
+        self.assertIsNone(lru.data['a']['next'], 'a is the last')
+
+    def test_read_throws_when_cache_miss(self):
+        lru = LRUCache(2)
+        with self.assertRaises(Exception) as notFound:
+            lru.read('a')
+        self.assertIsNotNone(notFound, 'Cache miss exception raised')
+
+    def test_read_returns_correct_value(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.write('b', 20)
+        expected = 20
+        actual = lru.read('b')
+        self.assertEqual(actual, expected, 'should return the correct value')
+
+    def test_read_must_rewire_the_first_pointer(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.write('b', 20)
+
+        lru.read('a')
+        self.assertEqual(lru.first['key'], 'a', 'a should now be the first')
+        self.assertIsNone(lru.first['previous'],
+            'first should not have anything before')
+        self.assertEqual(lru.first['next']['key'], 'b',
+            'b is not least recently used')
+        self.assertEqual(lru.last['key'], 'b', 'a should now be the first')
+        self.assertIsNone(lru.last['next'],
+            'last should not have anything after it')
+        self.assertEqual(lru.last['previous']['key'], 'a',
+            'a is before last')
+
+    def test_evict_when_cache_has_no_elements(self):
+        lru = LRUCache(2)
+        lru.evict()
+
+    def test_evict_when_cache_has_one_element(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.evict()
+        self.assertNotIn('a', lru.data, 'cache should be empty')
+        self.assertIsNone(lru.first, 'double linked list is empty')
+        self.assertIsNone(lru.last, 'double linked list is empty')
+
+    def test_evict_when_cache_has_at_least_two_elements(self):
+        lru = LRUCache(2)
+        lru.write('a', 10)
+        lru.write('b', 20)
+        lru.evict()
+
+        self.assertNotIn('a', lru.data, 'cache should not contain key a')
+        self.assertIn('b', lru.data, 'cache should contain key b')
+        self.assertEqual(lru.first['key'], 'b', 'cache should only contain b')
+        self.assertEqual(lru.last['key'], 'b', 'cache should only contain b')
+        self.assertIsNone(lru.last['previous'], 'no more data in cache')
+        self.assertIsNone(lru.last['next'], 'no more data in cache')
+
     def test_lru_evicts_old_data(self):
         lru = LRUCache(2)
         lru.write('a', 10)
-
         lru.write('b', 20)
-        time.sleep(0.1)
-
-        #import pdb; pdb.set_trace()
         lru.write('c', 30)
-        time.sleep(0.1)
 
         self.assertEqual(lru.read('b'), 20, 'should read the correct data')
         with self.assertRaises(Exception) as notFound:
