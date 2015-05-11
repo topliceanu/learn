e3c774c9dc1bf22a9787b4087c3a57cd3288d5dc
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-05-11 09:25:57 +0300

    add liniar complexity algorithm for maximum cut for bipartite graphs

--- algo/test/test_maximum_cut.py
+++ algo/test/test_maximum_cut.py
@@ -3,7 +3,7 @@
 import unittest
 
 from src.graph import Graph
-from src.maximum_cut import maximum_cut
+from src.maximum_cut import maximum_cut, maximum_cut_for_bipartite_graph
 
 
 class MaximumCut(unittest.TestCase):
@@ -26,3 +26,23 @@ class MaximumCut(unittest.TestCase):
         self.assertNotEqual(left, right, 'no common vertices between cuts')
         self.assertIn(set(left), expected, 'should correctly split the graph')
         self.assertIn(set(right), expected, 'should correctly split the graph')
+
+    def test_maximum_cut_for_bipartite_graphs(self):
+        """ Given the following bipartite graph.
+            (a)-----(b)
+              \
+               \----(c)
+            (d)-----(e)
+                    /
+            (f)----/
+              \
+               \----(g)
+        """
+        g = Graph.build(edges=[('a', 'b'), ('a', 'c'),
+                               ('d', 'e'), ('f', 'e'), ('f', 'g')],
+                        directed=False)
+
+        (left, right) = maximum_cut_for_bipartite_graph(g)
+        self.assertIn(len(left), [3,4], 'either 3 or 4')
+        self.assertIn(len(right), [3,4], 'eighter 3 or 4')
+        self.assertEqual(7, len(left)+len(right), 'no vertex counted twice')
