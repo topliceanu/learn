ee3ff2efc74118eaadf6d83361bcdea7905849ea
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-05-12 16:12:50 +0300

    introduce a depth first search algo which produces all paths possible.

--- algo/test/test_depth_first_search.py
+++ algo/test/test_depth_first_search.py
@@ -3,7 +3,7 @@
 import unittest
 
 from src.graph import Graph
-from src.depth_first_search import dfs
+from src.depth_first_search import dfs, dfs_paths
 
 
 
@@ -19,3 +19,21 @@ class DepthFirstSearch(unittest.TestCase):
                                ('b', 'c'), ('d', 'c'), ('d', 'e'), ('c', 'e')],
                         directed=False)
         path = dfs(g, 's')
+
+    def test_depth_first_search_returning_paths(self):
+        """ Given this graph to explore:
+            />(a)-->(d)-->(e)
+          /    |v    |v   /^
+        (s)-->(b)-->(c)-/
+        """
+        g = Graph.build(edges=[('s', 'a'), ('a', 'd'), ('d', 'e'), ('s', 'b'),
+                               ('b', 'c'), ('c', 'e'), ('a', 'b'), ('d', 'c')],
+                        directed=True)
+        paths = dfs_paths(g, 's', 'e')
+        expected_paths = [
+            ['s', 'b', 'c', 'e'], ['s', 'a', 'd', 'e'],
+            ['s', 'a', 'd', 'c', 'e'], ['s', 'a', 'b', 'c', 'e']
+        ]
+        self.assertEqual(paths, expected_paths,
+                'should produce the expected paths')
+
