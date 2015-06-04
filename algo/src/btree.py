c0ad48b9c96a68120a979d5f4163cef40b12aa28
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2015-06-04 13:43:45 +0300

    more work on the btree implementation

--- algo/src/btree.py
+++ algo/src/btree.py
@@ -7,30 +7,53 @@ from src.binary_search_tree import BST
 
 # TODO implement [Mond&Raz, 1985] to support a single pass-down.
 
-class Btree(object):
+class BTree(object):
     """ Acts as a facade to the root node of the BTree.
 
     Attrs:
-        root: object, instance of src.btree.BTreeNode
+        m: tree order, the number of children allowed for a node. Consequently
+           the a node can only host m-1 values.
+        root: object, src.btree.BTreeNode, root of the tree structure.
     """
-
     def __init__(self, m):
+        self.m = m
         self.root = BTreeNode(m)
 
+    def traverse(self):
+        """ Traverses all values in the tree in a structured order. """
+        return self.root.traverse()
+
+    def lookup(self, key):
+        """ Searches the value corresponding to the key. """
+        return self.root.lookup(key)
+
+    def insert(self, key, value):
+        """ Inserts a new value in the tree. This may lead to the formation of
+        a new root, which this method handles as well.
+        """
+        self.root.insert(key, value)
+
+        # Update the new root if need be.
+        node = self.root
+        while node.parent != None:
+            node = node.parent
+        self.root = node
+
     @classmethod
-    def build(cls, data, m):
+    def build(cls, m, data):
         """ Builds a new BTree given a dictionary of (key, value) pairs.
 
         The method used is called "bulkloading
 
         Args:
-            data: list, of tuples, format [(key, value)]
             m: int, order of the tree
+            data: list, of tuples, format [(key, value)]
 
         Returns:
             object, instance of src.btree.BTree
         """
-        data = sorted(data, key=0) # Make sure pairs are sorted by key.
+        # TODO
+        data = sorted(data, key=lambda x: x[0]) # Sort pairs by key.
         nodes = {} # Holds nodes and they governing value as key.
 
         while True:
@@ -40,11 +63,10 @@ class Btree(object):
             for chunk in chunks:
                 parent = chunk.pop()
                 data.append(parent)
-                node = BTreeNone(m)
+                node = BTreeNode(m)
                 node.keys = map(lambda i: i[0], chunk)
                 node.values = map(lambda i: i[0], chunk)
                 nodes[parent[0]] = node
-
 
 
 class BTreeNode(object):
@@ -53,13 +75,20 @@ class BTreeNode(object):
     Invariants for BTree of rank m:
     - every node has at most m children.
     - every non-leaf node (except for the root) has at least m/2 children.
-    - the root has at least two children if not a leaf.
-    - a non-leaf node with k children has k-1 keys.
-    - all leave are on the same leve.
+    - a node has at least two children if not a leaf.
+    - a non-leaf node with k-1 keys has k children.
+    - all keys in a node are sorted, and all keys in the tree are sorted.
+    - all leaves are on the same level.
+
+    Complexity: O(log n) for most operations (lookup, insert)
 
     References:
         http://www.cs.cornell.edu/courses/cs3110/2009sp/recitations/rec25.html
         https://gist.github.com/teepark/572734
+        http://www.geeksforgeeks.org/b-tree-set-1-introduction-2/ - proactive approach to insertion.
+        http://en.wikipedia.org/wiki/B-tree
+        http://www.semaphorecorp.com/btp/algo.html
+        http://www.bluerwhite.org/btree/
 
     Attrs:
         m: rank fo the node, ie. you are only allowed to m children and m-1 values.
@@ -81,9 +110,21 @@ class BTreeNode(object):
     def insert(self, key, value):
         """ Inserts a new value in the tree.
 
+        Algorithm:
+        - a new tree is always inserted at the leafs level, so first, lookup
+          the appropriate leaf, the insert the key.
+        - if this causes an overflow (ie. the node has more keys than the rank)
+          which leads to a node split.
+        - to split a node, find the median value of all values in the node,
+          insert that median into the parent node, create two new nodes, with
+          keys left and right of the median, and fix the parrent pointers to
+          point to the two new nodes.
+        - this may cause the parrent o overflow, in which case it must be split
+          also. This recurses up until there is no more parent that needs
+          splitting.
+
         Returns:
-            object, instance of src.btree.BTree, the node where the value was inserted.
-                which may have changed.
+            object, instance of src.btree.BTreeNode, the
         """
         # Find the leaf node where to do the insertion.
         if not self.is_leaf():
@@ -95,13 +136,73 @@ class BTreeNode(object):
         self.keys.insert(insert_point, key)
         self.values.insert(insert_point, value)
 
-        if not self.is_full():
-            return self
+        if self.is_full():
+            self.split()
 
-        self.split()
-        if self.parent == None:
-            return self
-        return self.parent.merge(self)
+        return self
+
+    def split(self):
+        """ Splits the node into two nodes.
+
+        The current node, keeps the left keys, the new sibling keeps the right
+        keys and the parent gets the median key.
+
+        If the parent overflows injecting the median, it is split as well, and
+        so on and so forth.
+
+        If the node being split is a root, a new root is created and pointer
+        wired correctly.
+
+        Args:
+            other: object, instance of src.btree.BTree, the other node to merge with this one.
+
+        Return:
+            object, instance of src.btree.BTree either the current node or the new root.
+        """
+        pos_median = len(self.keys)/2
+        key_median = self.keys[pos_median]
+        value_median = self.values[pos_median]
+        keys_left = self.keys[:pos_median]
+        keys_right = self.keys[pos_median+1:]
+        values_left = self.values[:pos_median]
+        values_right = self.values[pos_median+1:]
+        if self.is_leaf():
+            refs_left = []
+            refs_right = []
+        else:
+            refs_left = self.refs[:pos_median]
+            refs_right = self.refs[pos_median+1:]
+
+        # Update the current node.
+        self.keys = keys_left
+        self.values = values_left
+        self.refs = refs_left
+
+        # Create a new sibling with the right data.
+        sibling = BTreeNode(self.m)
+        sibling.parent = self.parent
+        sibling.keys = keys_right
+        sibling.values = values_right
+        sibling.refs = refs_right
+        for ref in sibling.refs:
+            ref.parent = sibling
+
+        # If the current node is root, we need to create a new root.
+        if self.is_root():
+            new_root = BTreeNode(self.m)
+            new_root.keys = [key_median]
+            new_root.values = [value_median]
+            new_root.refs = [self, sibling]
+            self.parent = new_root
+            sibling.parent = new_root
+        else:
+            index = self.parent.refs.index(self)
+            self.parent.keys.insert(index+1, key_median)
+            self.parent.values.insert(index+1, value_median)
+            self.parent.refs.insert(index+1, sibling)
+
+            if self.parent.is_full():
+                self.parent.split()
 
     def lookup(self, key):
         """ Find the first value in keys larger than input key.
@@ -121,10 +222,10 @@ class BTreeNode(object):
         if self.keys[k] == key:
             return node.values[k]
 
-        if self.refs[k] == None:
+        # Lookup in the child node.
+        if self.refs[k+1] == None:
             return None
-
-        return self.refs[k].lookup(key)
+        return self.refs[k+1].lookup(key)
 
     def traverse(self, recursive=False):
         """ Traverses all values of the node or the entire subtree.
@@ -136,12 +237,12 @@ class BTreeNode(object):
             list, of keys in the node (or the entire subtree)
         """
         out = []
-        for i in len(self.values)+1:
+        for i in range(len(self.keys)):
             if recursive == True and self.refs[i] != None:
                 out.extend(self.refs[i].traverse(recursive=True))
-            # When processing the last reference i+1, the value i was already added.
-            if i < len(self.values):
-                out.append[self.values[i]]
+            out.append[self.values[i]]
+        if recursive == True:
+            out.extend(self.refs[i+1].traverse(recursive=True))
         return out
 
     def delete(self, value):
@@ -180,65 +281,12 @@ class BTreeNode(object):
         return len(self.refs) == 0
 
     def is_root(self):
-        pass
+        """ A node is root only if it has no parent. """
+        return self.parent == None
 
     def get_position(self, key):
-        """ Method figures out which position in the keys list does key belong to. """
-        return bisect.bisect(self.keys, key)
+        """ Method figures out which position in the keys list does key belong to.
 
-    def split(self):
-        """ This method extracts two children from the current node.
-
-        The current node remains with only one value, the median of the keys in
-        the old set, while the two children contain keys left and right of the
-        median.
+        Finds and returns the first key that is greater than or equal to key.
         """
-        pos_median = len(self.keys)/2
-        keys_left = self.keys[:pos_median]
-        keys_right = self.keys[pos_median+1:]
-        values_left = self.values[:pos_median]
-        values_right = self.values[pos_median+1:]
-
-        left = BTree(self.m)
-        left.parent = self
-        left.keys = keys_left
-        left.values = values_left
-
-        right = BTree(self.m)
-        right.parent = self
-        right.keys = keys_right
-        right.values = values_right
-
-        self.keys = [self.keys[pos_median]]
-        self.values = [self.values[pos_median]]
-        self.refs = [left, right]
-
-    def merge(self, child):
-        """ Merges the current node with one of its children.
-
-        This method merges keys and values, maintaining the sorted order. The
-        references are also correctly inserted and merged.
-
-        If the node overflows after the merge, it is split and merged
-        recursively with it's parent.
-
-        Args:
-            other: object, instance of src.btree.BTree, the other node to merge with this one.
-
-        Return:
-            object, instance of src.btree.BTree either the current node or the new root.
-        """
-        for index, key in enumerate(other.keys):
-            insert_point = self.get_position(key)
-            self.keys.insert(insert_position, key)
-            self.values.insert(insert_position, other.values[index])
-            self.refs.insert(insert_position, other.refs[index])
-
-        if not self.is_full():
-            return self
-
-        self.split()
-        if self.is_root():
-            return self
-        else:
-            self.parent.merge(self)
+        return bisect.bisect_left(self.keys, key)
--- algo/test/test_btree.py
+++ algo/test/test_btree.py
@@ -2,9 +2,11 @@
 
 import unittest
 
-from src.btree import BTree, BTreeNode
+from src.btree import BTree
 
 
 class BtreeTest(unittest.TestCase):
 
-    pass
+    def test_build_should_construct_a_new_btree(self):
+
+        tree = BTree.build(3, [(2, 'a'), (-1, 'c'), (3, 'b'), (0, 'z')])
