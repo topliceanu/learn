f00f915fa19325e3e70bc70202ae0b6738588b4e
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2016-02-24 07:16:30 +0000

    introduce a new set of debug macros

--- c/learn_c_the_hard_way/dbg.h
+++ c/learn_c_the_hard_way/dbg.h
@@ -1,3 +1,4 @@
+// Debug Macros for C.
 #ifndef __dbg_h__
 #define __dbg_h__
 
@@ -10,26 +11,30 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+// debug() is a printf toghether with __FILE__, __LINE__ and __FUNCTION__
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d in %s(): " M "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 #endif
 
 // Produce a safe, readable version of errno.
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-// Logging functions for various severity errors.
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+// Logging functions for various severity errors. They work like printf but with __FILE__ and __LINE__
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d in %s(): errno: %s) " M "\n", __FILE__, __LINE__, __FUNCTION__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d in %s(): errno: %s) " M "\n", __FILE__, __LINE__, __FUNCTION__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d in %s()) " M "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 
-// Checks if condition A is true. If not, it logs the error, and jumps to the erro label of the function for cleanup.
+// Checks if condition A is true. If not, it logs the error, and jumps to the error label!? of the function for cleanup.
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
+// Place this in every part of the code that shouldn't run (and only runs because of an error). It jumps to the error label in the function.
 #define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
+// Checks that a pointer if valid!
 #define check_mem(A) check((A), "Out of memory.")
 
+// Same as `check` but without the error logging.
 #define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #endif
