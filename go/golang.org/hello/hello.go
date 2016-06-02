8248cb83b2bd4415c64ca19a9de37fbb2bbe4f41
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2016-06-02 19:58:34 +0100

    add code from the How To Write Go Code tutorial

---
+++ go/golang.org/hello/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "golang.org/stringutil"
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello world.\n"))
+}
---
+++ go/golang.org/stringutil/reverse.go
@@ -0,0 +1,10 @@
+// Package stringutil has methods for working with strings.
+package stringutil
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r) - 1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
