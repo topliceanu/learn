b5aec87980eb04e2592f563a2c5ab45188c5e9cc
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-03-19 19:49:58 +0000

    add signin form with validation

--- elm/elm-introduction/main.elm
+++ elm/elm-introduction/main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Html, button, div, text, ul, li, input)
+import Html exposing (Html, button, div, text, ul, li, input, h3)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 
@@ -6,21 +6,33 @@ import Html.Events exposing (onClick, onInput)
 type alias Model = {
   counter: Int,
   history: List String,
-  content: String
+  content: String,
+  name: String,
+  pass: String,
+  passAgain: String
 }
 
 model : Model
 model = {
     counter = 0,
     history = [ "initial" ],
-    content = ""
+    content = "",
+    name = "",
+    pass = "",
+    passAgain = ""
   }
 
 -- update
-type Msg = Increment
+type Msg
+  -- counter
+  = Increment
   | Decrement
   | Reset
   | Change String
+  -- register form, the inputs will be changed separately
+  | Name String
+  | Pass String
+  | PassAgain String
 
 -- limits the number of items in a list
 addToHistory : String -> List String -> List String
@@ -38,21 +50,49 @@ update msg model =
       { model | counter = 0, history = addToHistory "reset" model.history }
     Change newContent ->
       { model | content = newContent, history = addToHistory "content changed" model.history }
+    Name newName ->
+      { model | name = newName, history = addToHistory "name changed" model.history }
+    Pass newPass ->
+      { model | pass = newPass, history = addToHistory "password changed" model.history }
+    PassAgain newPassAgain ->
+      { model | passAgain = newPassAgain, history = addToHistory "re-entered password changed" model.history }
 
--- view
+-- renders the history as a UL of LIs
 showHist : List String -> Html Msg
 showHist history =
   ul [] (List.map (\h -> li [] [text h]) history)
 
+viewValidation : Model -> Html msg
+viewValidation model =
+  let (color, message) =
+    if model.pass == model.passAgain then
+      ("green", "All OK!")
+    else
+      ("red", "Passwords do not match")
+  in
+    div [ style [ ("color", color) ] ] [ text message ]
+
+-- view
 view : Model -> Html Msg
 view model =
   div [] [
+    h3 [] [ text "Signup Form" ],
+    input [ type_ "test", placeholder "Name", onInput Name ] [],
+    input [ type_ "password", placeholder "Password", onInput Pass ] [],
+    input [ type_ "password", placeholder "Re-enter password", onInput PassAgain ] [],
+    viewValidation model,
+
+    h3 [] [ text "Reversing Text" ],
     input [ placeholder "Text to reverse", onInput Change ] [],
     div [] [ text (String.reverse model.content) ],
+
+    h3 [] [ text "Counter" ],
     button [ onClick Decrement ] [ text "-" ],
     div [] [ text (toString model.counter) ],
     button [ onClick Increment ] [ text "+" ],
     button [ onClick Reset ] [ text "0" ],
+
+    h3 [] [ text "history" ],
     showHist(model.history)
   ]
 
