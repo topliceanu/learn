38e470d8f1ce48edf0961f184531c2a87ff94a56
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-03-19 18:37:12 +0000

    add history of increments/decrements/resets

--- elm/elm-introduction/main.elm
+++ elm/elm-introduction/main.elm
@@ -1,11 +1,17 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, ul, li)
 import Html.Events exposing (onClick)
 
 -- model
-type alias Model = Int
+type alias Model = {
+  counter: Int,
+  history: List String
+}
 
 model : Model
-model = 0
+model = {
+    counter = 0,
+    history = [ "initial" ]
+  }
 
 -- update
 type Msg = Increment | Decrement | Reset
@@ -14,20 +20,34 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      {
+        counter = model.counter + 1,
+        history = "inc" :: model.history
+      }
     Decrement ->
-      model - 1
+      {
+        counter = model.counter - 1,
+        history = "dec" :: model.history
+      }
     Reset ->
-      0
+      {
+        counter = 0,
+        history = "reset" :: model.history
+      }
 
 -- view
+showHist : List String -> Html Msg
+showHist history =
+  ul [] (List.map (\h -> li [] [text h]) history)
+
 view : Model -> Html Msg
 view model =
   div [] [
-    button [onClick Decrement] [text "-"],
-    div [] [text (toString model)],
-    button [onClick Increment] [text "+"],
-    button [onClick Reset] [text "0"]
+    button [ onClick Decrement ] [ text "-" ],
+    div [] [ text (toString model.counter) ],
+    button [ onClick Increment ] [ text "+" ],
+    button [ onClick Reset ] [ text "0" ],
+    showHist(model.history)
   ]
 
 -- application
