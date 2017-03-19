4d88b83a8b781b77852979f81cb35aeb767c4470
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-03-19 18:17:39 +0000

    first implementation of the increment

---
+++ elm/elm-introduction/main.elm
@@ -0,0 +1,37 @@
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+-- model
+type alias Model = Int
+
+model : Model
+model = 0
+
+-- update
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+
+-- view
+view : Model -> Html Msg
+view model =
+  div [] [
+    button [onClick Decrement] [text "-"],
+    div [] [text (toString model)],
+    button [onClick Increment] [text "+"]
+  ]
+
+-- application
+main =
+  Html.beginnerProgram {
+    model = model,
+    view = view,
+    update = update
+  }
+
