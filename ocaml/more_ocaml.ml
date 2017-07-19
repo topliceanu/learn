b108d5c746159d6c230e332575bf91c87c9f61e2
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-07-19 20:04:26 +0100

    add interleave function

--- ocaml/more_ocaml.ml
+++ ocaml/more_ocaml.ml
@@ -43,3 +43,24 @@ let rec lfilter p (Cons (hd, lt)) =
     if p hd = true
     then Cons (hd, rest)
     else (rest ())
+
+(* cubes divisible by 5 *)
+let cubes =
+  lseq 1 |>
+  lmap (fun x -> x * x * x) |>
+  lfilter (fun x -> x mod 5 = 0)
+  (*
+  let src = lseq 1 in
+  let cbs = lmap (fun x -> x * x * x) src in
+  lfilter (fun x -> x mod 5 = 0) cbs
+  *)
+
+(* filters the input seed list to return all primes *)
+(* mkprimes : 'a lazylist -> 'a lazylist *)
+let rec mkprimes (Cons (hd, tl)) =
+  Cons (hd, fun () ->
+    mkprimes (lfilter (fun x -> x mod hd <> 0) (tl ())))
+
+(* interleaves elements from two lazy lists between each-other. *)
+let rec interleave (Cons (hd1, lt1)) l2 =
+  Cons (hd1, fun () -> interleave l2 (lt1 ()))
