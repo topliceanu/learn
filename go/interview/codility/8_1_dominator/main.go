396f72825fa0901ed1e0d6d17402e86e4a410d9e
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2018-03-31 09:53:18 +0100

    solution to the dominator problem, however it fails one test

---
+++ go/interview/codility/8_1_dominator/main.go
@@ -0,0 +1,94 @@
+package main
+
+/*
+1. think of pre-processing steps: sort, arrange the data, index the data, prefix sums!
+2. split into small functions which you will implement later
+3. solution scanning and offer alternatives (always talk about complexity in space and time)
+	1. pattern matching (find similar problems)
+	2. simplify and generalize (start with a simpler problem)
+	3. iterate through programming paradigms (greedy, divide and conquer, dynamic programming)
+	4. iterate through all data structures (lists, arrays, stacks, queues, heap, hash, tree, trie, bloom filter, union_find)
+	5. try free primitive and see if you make progress (sorting, bfs, dfs, strongly connected components, shortest path)
+4. BUD optimisation:
+	1. bottleneck
+	2. unnecessary work
+	3. duplicate work
+5. identify pain points: array indices, loop termination conditions.
+*/
+
+import "fmt"
+import "math"
+
+func Solution(A []int) int {
+	stack := empty
+	for _, a := range A {
+		stack = stack.push(a)
+		isPartial, b, c := stack.doublePeek()
+		if !isPartial && b != c {
+			_, _, stack = stack.pop()
+			_, _, stack = stack.pop()
+		}
+	}
+	isEmpty, candidate := stack.peek()
+	if isEmpty {
+		return -1
+	}
+	count := 0
+	index := 0
+	for i, a := range A {
+		if a == candidate {
+			count += 1
+			index = i
+		}
+	}
+	if float64(count) >= math.Ceil(float64(len(A)) / 2) {
+		return index
+	}
+	return -1
+}
+
+type node struct {
+	val int
+	next *node
+}
+
+var empty = &node{0, nil}
+
+func (n *node) debug() string {
+	if n == empty {
+		return "|"
+	}
+	return fmt.Sprintf("%d->%s", n.val, n.next.debug())
+}
+
+func (n *node) peek() (isEmpty bool, val int) {
+	if n == empty {
+		return true, 0
+	}
+	return false, n.val
+}
+
+func (n *node) pop() (isEmpty bool, val int, head *node) {
+	if n == empty {
+		return true, 0, empty
+	}
+	return false, n.val, n.next
+}
+
+func (n *node) push(val int) *node {
+	return &node{val, n}
+}
+
+func (n *node) doublePeek() (isPartial bool, h1, h2 int) {
+	isEmpty, h1 := n.peek()
+	if isEmpty {
+		return true, 0, 0
+	}
+	isEmpty, h2 = n.next.peek()
+	if isEmpty {
+		return true, h1, 0
+	}
+	return false, h1, h2
+}
+
+func main() {}
---
+++ go/interview/codility/8_1_dominator/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+/*
+1. imagine the usual scenario
+2. identify general edge cases
+3. identify edge cases specific on the implementation
+*/
+
+func TestSolution(t *testing.T) {
+	for i, tc := range []struct{
+		A []int
+		expected int
+	}{
+		// their examples.
+		{[]int{6, 8, 4, 6, 8, 6, 6}, 6},
+		{[]int{3, 4, 3, 2, 3, -1, 3, 3}, 7},
+		{[]int{1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2}, -1},
+		// for even length arrays.
+		{[]int{1, 1, 1, 2, 3, 3}, -1},
+		// no leader
+		{[]int{1, 1, 2, 2, 3, 3}, -1},
+		// negative numbers give out the same value as when no leader is found?!
+		{[]int{-1, -1, -1, -2, -3}, 2},
+		// edge cases
+		{[]int{}, -1},
+		{[]int{1}, 0},
+		{[]int{1, 1, 1, 1, 1}, 4},
+	}{
+		actual := Solution(tc.A)
+		if actual != tc.expected {
+			t.Errorf("case %d: (%v) expected %d, got %d", i, tc.A, tc.expected, actual)
+		}
+	}
+}
