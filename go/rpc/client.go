413efbea80bc7d3463d042c96aeca270005b4324
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2019-01-06 16:36:03 +0000

    test go's net/rpc package for a simple RPC interface between two golan processes

---
+++ go/rpc/client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/rpc"
+	"fmt"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", ":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	args := &Args{7,8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+	// Asynchronous call
+	quotient := new(Quotient)
+	divCall := client.Go("Arith.Divide", args, quotient, nil)
+	replyCall := <-divCall.Done	// will be equal to divCall
+	fmt.Printf("Arith: %d/%d=%#v", args.A, args.B, replyCall.Reply)
+}
---
+++ go/rpc/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net"
+	"net/rpc"
+	"net/http"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	http.Serve(l, nil)
+}
