692be4d982619952187011d64a24543286248eee
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2020-05-01 13:07:45 +0100

    renamed the ocaml algo folder into cci-ed6 book because that's where the exercises are sourced from

--- ocaml/cci-ed6/sorting_and_searching.ml
+++ ocaml/cci-ed6/sorting_and_searching.ml
@@ -1,5 +1,7 @@
 (* val merge : 'a list -> 'a list -> 'a list
- * CCI book, 5th edition, problem 9.1
+ * CCI book, 5th edition, problem 11.1:
+ * You are given two sorted arrays, A and B, where A has a large enough buffer
+ * at the end to hold B. Write a method to merge B and A in sorted order.
  **)
 let rec merge l1 l2 =
   match (l1, l2) with
@@ -9,4 +11,3 @@ let rec merge l1 l2 =
   | (x :: xs, y :: ys) ->
       if x < y then x :: (merge xs (y :: ys))
       else y :: (merge (x :: xs) ys)
-
