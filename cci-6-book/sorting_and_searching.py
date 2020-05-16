976cca2acd6b43da2deae31bd85a402c01991b68
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2020-05-16 13:00:26 +0100

    add more exercises from chapter 10: sorting and searching

--- cci-6-book/sorting_and_searching.py
+++ cci-6-book/sorting_and_searching.py
@@ -2,6 +2,152 @@
 
 import heapq
 
+def sorted_merge(a, b):
+    """ 10.1 Sorted Merge: You are given two sorted arrays, A and B, where A
+    has a large enough buffer at the end to hold B. Write a method to merge B
+    into A in sorted order.
+    """
+    if len(a) == 0 and len(b) == 0:
+        return []
+    if len(a) == 0:
+        return b
+    if len(b) == 0:
+        return a
+    ha, ta = a[0], a[1:]
+    hb, tb = b[0], b[1:]
+    if ha < hb:
+        return [ha] + sorted_merge(ta, b)
+    else:
+        return [hb] + sorted_merge(a, tb)
+
+def group_anagrams(words):
+    """ 10.2 Group Anagrams: Write a method to sort an array of strings so that
+    all the anagrams are next to each other.
+    """
+    words_with_sorted = [(w, str(sorted(list(w)))) for w in words]
+    words_sorted_by_anagram = sorted(words_with_sorted, key=lambda p: p[1])
+    return [p[0] for p in words_sorted_by_anagram]
+
+def search_rotated(arr, el):
+    """ 10.3 Search in Rotated Array: Given a sorted array of n integers that
+    has been rotated an unknown number of times, write code to find an element
+    in the array. You may assume that the array was originally sorted in
+    increasing order.
+    EXAMPLE
+    Input: find 5 in {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
+    Output: 8 (the index of 5 in the array)
+    """
+    def search(arr, el, left, right):
+        if left > right:
+            return None
+        if left == right:
+            if arr[left] == el:
+                return left
+            else:
+                return None
+        middle  = (left + right) / 2
+        if arr[left] <= arr[middle]: # inflection is to the right
+            if el <= arr[middle]:
+                return search(arr, el, left, middle)
+            else:
+                return search(arr, el, middle+1, right)
+        else: # inflection is the left side
+            if el >= arr[middle]:
+                return search(arr, el, middle, right)
+            else:
+                return search(arrl, el, left, middle-1)
+
+    return search(arr, el, 0, len(arr) - 1)
+
+def sorted_search_no_size(x, elementAt):
+    """ 10.4 Sorted Search, No Size: You are given an array-like data structure
+    Listy which lacks a size method. It does, however, have an elementAt(i)
+    method that returns the element at index i in 0(1) time. If i is beyond the
+    bounds of the data structure, it returns -1. (For this reason, the data
+    structure only supports positive integers.) Given a Listy which contains
+    sorted, positive integers, find the index at which an element x occurs.
+    If x occurs multiple times, you may return any index.
+    """
+    def rec_search(el, elementAt, left_idx, right_idx):
+        left_val = elementAt(left_idx)
+        right_val = elementAt(right_idx)
+        if left_idx == right_idx:
+            if el == left_val:
+                return left_idx
+            else:
+                return None
+        if right_val == -1:
+            return rec_search(el, elementAt, left_idx, (left_idx + right_idx) / 2)
+        if el < right_val:
+            return rec_search(el, elementAt, left_idx, (left_idx + right_idx) / 2)
+        if right_val < el:
+            return rec_search(el, elementAt, right_idx, right_idx * 2)
+        if right_val == el:
+            return right_idx
+
+    return rec_search(x, elementAt, 0, 100)
+
+def sparse_search(arr, s):
+    """ 10.5 Sparse Search: Given a sorted array of strings that is interspersed
+    with empty strings, write a method to find the location of a given string.
+    EXAMPLE:
+    Input: find "ball" in {"at", "", "", "" , "ball", "", "", "car", "" , "" , "dad", ""}
+    Output: 4
+    """
+    def spread(arr, middle, left, right):
+        k = 1
+        while middle - k >= left and middle + k <= right:
+            if arr[middle - k] != "":
+                return middle - k
+            if arr[middle + k] != "":
+                return middle + k
+            k += 1
+        return middle
+    def rec_sparse_search(arr, s, left, right):
+        if left > right:
+            return None
+        middle = (left + right) / 2
+        if arr[middle] == "":
+            new_middle = spread(arr, middle, left, right)
+            if new_middle == middle:
+                return None
+            middle = new_middle
+        if arr[middle] == s:
+            return middle
+        if arr[middle] < s:
+            return rec_sparse_search(arr, s, left, middle - 1)
+        return rec_sparse_search(arr, s, middle + 1, right)
+
+    return rec_sparse_search(arr, s, 0, len(arr) - 1)
+
+def sort_big_file(lines):
+    """ 10.6 Sort Big File: Imagine you have a 20 GB file with one string per
+    line. Explain how you would sort the file.
+
+    Solution: bucket sort, we make a pass through each line and copy it at the
+    an of new file  that starts with the first two letters of the line.
+    At the end we will have 24*2 file, of more-or-less 0.5GB, which can be sorted in memory.
+    Finally, merge the files in alphabetic order on disk.
+    """
+    buckets = {}
+    for line in lines:
+        prefix = line[:2]
+        if prefix not in buckets:
+            buckets[prefix] = []
+        buckets[prefix].append(line)
+
+    for key, bucket in buckets.items():
+        buckets[key] = sorted(bucket)
+
+    sorted_by_keys = sorted(buckets.items(), key=lambda p: p[0])
+
+    output = []
+    for _, bucket in sorted_by_keys:
+        output = output + bucket
+    return output
+
+## PRACTICE BASE ALGORITHMS
+
 def bubble_sort(arr):
     """ Time complexity O(n^2) Space complexity O(1) """
     for i in range(len(arr) - 1):
--- cci-6-book/test_sorting_and_searching.py
+++ cci-6-book/test_sorting_and_searching.py
@@ -3,9 +3,78 @@
 import unittest
 
 from sorting_and_searching import bubble_sort, selection_sort, \
-    merge_sort, quick_sort, heap_sort
+    merge_sort, quick_sort, heap_sort, sorted_merge, group_anagrams, \
+    search_rotated, sorted_search_no_size, sparse_search, sort_big_file
 
 class TestSortingAndSearch(unittest.TestCase):
+    def test_sorted_merge(self):
+        tests = [
+            ([1,3,5], [2,4,6], [1,2,3,4,5,6])
+        ]
+        for test in tests:
+            actual = sorted_merge(test[0], test[1])
+            expected = test[2]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
+    def test_group_anagrams(self):
+        tests = [
+            (["pot", "lea", "top", "ela"], ["lea", "ela", "pot", "top"]),
+        ]
+        for test in tests:
+            actual = group_anagrams(test[0])
+            expected = test[1]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
+    def test_search_rotated(self):
+        tests = [
+            ([1, 2, 3, 4, 5], 5, 4),
+            ([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 5, 8),
+            ([8, 9, 1, 2, 3, 4, 5, 6, 7], 5, 6),
+            ([3, 4, 5, 6, 7, 8, 9, 1, 2], 5, 2),
+        ]
+        for test in tests:
+            actual = search_rotated(test[0], test[1])
+            expected = test[2]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
+    def test_sorted_search_no_size(self):
+        def elementAt(arr, idx):
+            if idx >= len(arr):
+                return -1
+            else:
+                return arr[idx]
+        def makeElementAt(arr):
+            return lambda idx: elementAt(arr, idx)
+        tests = [
+            ([1, 2, 3, 4, 5], 5, 4),
+            ([1, 1, 2, 3, 3, 3, 4, 5, 7, 10, 14, 15, 16, 19, 20, 25], 7, 8),
+            ([1, 2, 3, 4, 5, 6, 7, 8, 9], 8, 7),
+        ]
+        for test in tests:
+            actual = sorted_search_no_size(test[1], makeElementAt(test[0]))
+            expected = test[2]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
+    def test_sparse_search(self):
+        tests = [
+            (["at", "", "", "" , "ball", "", "", "car", "" , "" , "dad", ""], "ball", 4),
+            (["", "", "" , "", "", "car", "" , "" , "dad", ""], "ball", None),
+        ]
+        for test in tests:
+            actual = sparse_search(test[0], test[1])
+            expected = test[2]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
+    def test_sort_big_file(self):
+        tests = [
+            (["at", "ale", "bizz", "ball", "car", "dud", "dad"],
+             ["ale", "at", "ball", "bizz", "car", "dad", "dud"]),
+        ]
+        for test in tests:
+            actual = sort_big_file(test[0])
+            expected = test[1]
+            self.assertEqual(actual, expected, 'failed test={} with actual={}'.format(test, actual))
+
     def test_sort(self):
         fns = {
             "bubble": (lambda arr: bubble_sort(arr)),
