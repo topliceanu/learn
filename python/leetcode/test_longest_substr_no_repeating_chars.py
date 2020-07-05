ee782bf140546e9f5940fbf6f2392d7cdf678773
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2020-07-05 09:07:17 +0100

    refactor longest substring solution and test

--- python/leetcode/test_longest_substr_no_repeating_chars.py
+++ python/leetcode/test_longest_substr_no_repeating_chars.py
@@ -2,10 +2,7 @@
 
 import unittest
 
-from longest_substr_no_repeating_chars import Solution
-
-s = Solution()
-global s
+from longest_substr_no_repeating_chars import length_of_longest_substr
 
 class TestLongestSubstrNoRepeatingChars(unittest.TestCase):
 
@@ -19,7 +16,7 @@ class TestLongestSubstrNoRepeatingChars(unittest.TestCase):
             ("abbbbb", 2),
         ]
         for test in tests:
-            actual = s.lengthOfLongestSubstring(test[0])
+            actual = length_of_longest_substr(test[0])
             expected = test[1]
             self.assertEqual(actual, expected, \
                 'failed test={} with actual={}'.format(test, actual))
